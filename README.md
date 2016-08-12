a9e78a11ebc0d6e153618777fe4f0e34a0f7b5a8
Nathan Schwartz <nathan.schwartz95@gmail.com>
2016-08-12 12:34:18 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Crosshair App
+
+This is an Overwolf app. Overwolf is a program for Windows that allows you to use apps in an overlay when you are gaming.
+
+This one gives you a crosshair. It used to allow toggling for ADS, but unfortunately the 
+methods available were too laggy to be worth it.
+
+The recenter and pixel adjustment buttons only work in game since they are dependant on game window resolution.
+Hotkeys (hide/show and close) also only work in game. These can be configured in Overwolf settings.
+
+To install:
+Download this folder from Google Drive.
+Download Overwolf.
+Open Overwolf.
+Go to the settings page.
+Under the support tab there should be a "Development options" button, click on it.
+In the new window that pops up, click Load unpacked extension.
+In the File browser, select the crosshair folder you are currently inside of.
+
+The app should automatically be added to your Overwolf menu.
---
+++ src/crosshair.js
@@ -0,0 +1,238 @@
+(function(){
+  
+  //@TODO: Find better image loading system?
+  //@TODO: Allow user to select personal crosshair?
+  
+  /*
+  overwolf.utils.openFilePicker(filter, callback)
+  Version added: 0.91.100
+  Opens a file picker dialog to browse for a file. A url to the selected file will be returned.
+  Permissions required: FileSystem
+  Parameter: filter   <string>
+  A file filter. Supports wild cards (*) and seperated by commas (,). Ex. myFile*.*,*.txt
+  Parameter: callback <function>
+  Called with a url to the selected file.
+  Callback argument:
+  {
+      "status": "success",
+      "url": "overwolf-fs://E/Video"
+  }
+  */
+
+  var appPath = "";
+  var selected = false;
+  var windowID;
+  var url = "";
+
+  $(document).ready(function(){
+    init();
+  });
+  
+
+  // Listeners
+  $("#drop").change(function(){setXPreview(document.getElementById("drop").value);});
+  $("#hide").mousedown (function(){
+    setX(); 
+    document.getElementById("CrosshairContainer").style.backgroundColor = 'transparent'; 
+    document.getElementById("CrosshairContainer").style.borderImageWidth = "0px";
+    document.getElementById("form").style.left = "25px";
+    document.getElementById("form").style.top = "52px";
+    document.getElementById("leftAdjust").style.display = "none";
+    document.getElementById("rightAdjust").style.display = "none";
+    document.getElementById("downAdjust").style.display = "none";
+    document.getElementById("upAdjust").style.display = "none";
+    document.getElementById("recenter").style.display = "none";
+    document.getElementById("adjustLabel").style.display = "none";
+    selected = true;
+  });
+  $("#leftAdjust").click(function(){
+    overwolf.windows.getCurrentWindow(function(results){
+      overwolf.windows.changePosition(windowID, results.window.left-1, results.window.top);
+    });
+  });
+  $("#rightAdjust").click(function(){
+    overwolf.windows.getCurrentWindow(function(results){
+      overwolf.windows.changePosition(windowID, results.window.left+1, results.window.top);
+    });
+  });
+  $("#downAdjust").click(function(){
+    overwolf.windows.getCurrentWindow(function(results){
+      overwolf.windows.changePosition(windowID, results.window.left, results.window.top+1);
+    });
+  });
+  $("#upAdjust").click(function(){
+    overwolf.windows.getCurrentWindow(function(results){
+      overwolf.windows.changePosition(windowID, results.window.left, results.window.top-1);
+    });
+  });
+  $("#CrosshairContainer").mousedown(function(e){
+    if (!$(e.target).hasClass('noDrag') && !selected) 
+      dragMove();
+  });
+  $("#recenter").click(function(){
+    overwolf.games.getRunningGameInfo(function(result){
+      overwolf.windows.changePosition(windowID, (result.logicalWidth/2)-101, (result.logicalHeight/2)-133);
+    });
+  });
+
+  
+  // Initialize App stuffs.
+  function init(){
+    setXPreview(document.getElementById("drop").value);
+    
+    // Set windowID
+    overwolf.windows.obtainDeclaredWindow("Crosshair",
+      function(result) {
+        if (result.status == "success") {
+          windowID = result.window.id;
+        }
+      }
+    );
+
+    // Show window
+    $("#CrosshairContainer").fadeIn();
+
+    // Register Hotkeys
+    overwolf.settings.registerHotKey("showHide", function(arg) {
+      if (arg.status == "success") {
+        overwolf.windows.getCurrentWindow(function(window) {
+          if (window.window.isVisible === true) {
+            minimizeWindow();
+          } else {
+            restoreWindow();
+          }
+        });
+      }
+    });
+    
+    overwolf.settings.registerHotKey("close", function(arg) {
+      if (arg.status == "success") {
+        closeWindow();
+      }
+    });
+
+    // Use IO plugin if available
+    (plugin() == null) 
+      ? console.log("Plugin couldn't be loaded??") 
+      : getMyWebAppDirectory(getDirectory);
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /////////// HELPER FUNCTIONS //////////
+
+  // Overwolf Window Helper functions
+  function dragMove() {
+    overwolf.windows.dragMove(windowID);
+  };
+  function closeWindow() {
+    overwolf.windows.close(windowID);
+  };
+  function minimizeWindow() {
+    overwolf.windows.minimize(windowID);
+  };
+  function restoreWindow() {
+    overwolf.windows.restore(windowID);
+  };
+  function dragResize(edge) {
+    overwolf.windows.dragResize(windowID, edge);
+  };
+
+
+  // IO Plugin helpers
+  function plugin() {
+    return document.querySelector('#plugin');
+  }
+  function getDirectory(mypath, status){
+    appPath = mypath;
+  };
+  function getMyWebAppDirectory(callback) {
+    var mypath = plugin().LOCALAPPDATA + "\\Overwolf\\Extensions";
+    var fallback = function() {
+      var pluginStr = window.location.host;
+      pluginStr = pluginStr.replace("Window_Extension_", "");
+      var index = pluginStr.lastIndexOf("_");
+      if (index != -1) {
+        pluginStr = pluginStr.slice(0,index);
+      }
+      mypath += "\\" + pluginStr + "\\" + "1.0.0";
+    }
+    overwolf.extensions.current.getManifest(function(manifestObj) {
+      if (typeof manifestObj === "undefined") {
+        console.error("Could not get Manifest.");
+        fallback();
+      } else if (typeof manifestObj.UID === "undefined") {
+        console.error("Manifest has no UID.");
+        fallback();
+      } else if (typeof manifestObj.meta === "undefined") {
+        console.error("Manifest has no meta object.");
+        fallback();
+      } else {
+        mypath += "\\" + manifestObj.UID + "\\" + manifestObj.meta.version;
+      }
+      plugin().isDirectory( 
+        mypath, 
+        function(status) {
+          callback(mypath, status);
+        }
+      );
+    });
+  };
+ 
+  // Set preview crosshair to new image.
+  function setXPreview(value, name){
+    switch(value){
+      case "default1":
+        url = "<img src='../images/default1.png' id='reticle'>";
+        break;
+      case "default2":
+        url = "<img src='../images/default2.png' id='reticle'>";
+        break;
+      case "default3":
+        url = "<img src='../images/default3.png' id='reticle'>";
+        break;
+      case "default4":
+        url = "<img src='../images/default4.png' id='reticle'>";
+        break;
+      case "default5":
+        url = "<img src='../images/default5.png' id='reticle'>";
+        break;
+      case "default6":
+        url = "<img src='../images/default6.png' id='reticle'>";
+        break;
+      case "default7":
+        url = "<img src='../images/default7.png' id='reticle'>";
+        break;
+      case "default8":
+        url = "<img src='../images/default8.png' id='reticle'>";
+        break;
+      case "default9":
+        url = "<img src='../images/default9.png' id='reticle'>";
+        break;
+      case "custom":
+        url = "<img src='../images/customX3.png' id='reticle'>";
+        
+        overwolf.utils.openFilePicker('*.png*', function(result){
+          if(result.status === "success"){
+            url = "<img src='" + result.url + "' id='reticle'>";
+            document.getElementById("preview").innerHTML = url;
+          }
+        })
+        break;        
+    } 
+    document.getElementById("preview").innerHTML = url;
+  };
+
+  // Set crosshair and remove options.
+  function setX(){
+    document.getElementById("form").innerHTML = url;
+    $( "#preview" ).remove();
+  };
+  
+})();
